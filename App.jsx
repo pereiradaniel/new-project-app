3d01b9d7fb2db582216fd098bdd53e9dbb5a88eb
Daniel <truenorthtrainingcompany@gmail.com>
2016-03-12 17:30:38 -0500

    Render App component App component is rendered on Meteor.startup

---
+++ App.jsx
@@ -0,0 +1,12 @@
+// App component - represents the whole app
+App = React.createClass({
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>#App component</h1>
+        </header>
+      </div>
+    );
+  }
+});
