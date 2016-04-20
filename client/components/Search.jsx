7e0617f5d2910df012452980f13dd57c91b23372
Daniel <truenorthtrainingcompany@gmail.com>
2016-04-19 21:09:43 -0400

    Refactoring search and select components

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -2,39 +2,61 @@
 Search = React.createClass({
   mixins: [ReactMeteorData],
 
+  getInitialState() {
+    return {
+      showCards: false
+    }
+  },
+
   getMeteorData() {
     if ( Customers.findOne({userId: {$not: {$ne: Meteor.userId()}}}) ) {
-      return { userProfile: Customers.findOne({userId: {$not: {$ne: Meteor.userId()}}}) };
+      return {userProfile: Customers.findOne({userId: {$not: {$ne: Meteor.userId()}}}) };
     } else if ( Providers.find({userId: {$not: {$ne: Meteor.userId()}}}) ) {
       return { userProfile: Providers.findOne({userId: {$not: {$ne: Meteor.userId()}}}) };
     }
   },
-	handleSubmit(event) {
-		event.preventDefault();
-      ReactLayout.render(MainLayout, {
-        content: <SelectProvider userProfile={this.data.userProfile} />
-      });
-	},
-  render() {
-    return (
-      <div>
-        <h1>#Search</h1>
-        <h3>(map is displayed)</h3>
-        <h3>{ this.data.userProfile.name }</h3>
-        { this.data.userProfile.userType === "customer" ?
+  renderSearchResults() {
+    return this.state.searchResults.map((provider) => {
+      return <Provider
+        key={provider._id}
+        provider={provider} />;
+    });
+  },
+  renderSearchForm() {
+    if (this.data.userProfile.userType === "customer") {
+      return (
           <form className="search" onSubmit={this.handleSubmit} >
-          	Number of Hours Required
+            Number of Hours Required
             <input type="number" ref="hoursInput" />
 
-          	<button>SEARCH</button>
-        </form> :
+            <button>SEARCH</button>
+          </form>
+        )
+    } else {
+      return (
         <form className="search" onSubmit={this.handleSubmit} >
             Distance Limit in Kilometers
             <input type="number" ref="hoursInput" />
 
             <button>SEARCH</button>
         </form>
-      }
+      )
+    }
+  },
+	handleSubmit(event) {
+		event.preventDefault();
+    this.setState({
+      searchResults: Providers.find({available: {$ne: false}}).fetch(),
+      showCards: ! this.state.showCards
+    });
+	},
+  toggleCards() {
+    this.setState({ showCards: ! this.state.showCards });
+  },
+  render() {
+    return (
+      <div>
+        {this.state.showCards === true ? this.renderSearchResults() : this.renderSearchForm() }
       </div>
     );
   }
